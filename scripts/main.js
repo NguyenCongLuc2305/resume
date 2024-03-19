193f8fe736b07bf367f781ee864395748e7f9a31
Nguyễn Công Lực <congluc2305@gmail.com>
2024-03-19 10:46:24 +0700

    luc up resume

--- scripts/main.js
+++ scripts/main.js
@@ -77,7 +77,7 @@
     var img = e.querySelector('img')
     // set the link present on the item to the caption in full view
     img.dataset.caption = '<a class="link-light" target="_blank" href="' + e.href + '">' + caption.innerHTML + '</a>';
-    window.console.log(caption, img)
+    // window.console.log(caption, img)
      e.addEventListener("click", (function(t){
        t.preventDefault();
        BigPicture({
@@ -89,6 +89,61 @@
   }))
 
   // Add your javascript here
+
+
+  window.addEventListener('scroll', function() {
+
+    var isInViewportCenter = function(elem) {
+      var bounding = elem.getBoundingClientRect();
+      var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+      var elementTop = bounding.top;
+      var elementBottom = bounding.bottom;
+
+      // Tính toán vị trí của phần tử so với vị trí giữa màn hình
+      var elementCenter = (elementTop + elementBottom) / 2;
+      var viewportCenter = viewportHeight / 2;
+
+      // Kiểm tra nếu phần tử nằm trong khoảng giữa màn hình
+      return Math.abs(elementCenter - viewportCenter) < viewportHeight / 4; // Điều chỉnh giá trị 4 cho mức độ nhạy cảm
+  };
+
+    var markers = document.querySelectorAll('h2.marker-center');
+
+    markers.forEach(function(marker) {
+        
+        if (isInViewportCenter(marker)) {
+            marker.classList.add('active-divider');
+        } 
+        // else {
+        //     marker.classList.remove('active-divider');
+        // }
+    });
+
+
+   // Add class to menu links based on corresponding section positions
+   var sections = document.querySelectorAll('.section');
+   var menuLinks = document.querySelectorAll('.navbar-nav li a');
+
+   menuLinks.forEach(function(menuLink) {
+       var sectionId = menuLink.getAttribute('href').substring(1);
+       var section = document.getElementById(sectionId);
+       if (isInViewportCenter(section)) {
+           menuLink.classList.add('active-menu');
+       } else {
+           menuLink.classList.remove('active-menu');
+       }
+   });
+
+});
+
+window.addEventListener('load', function() {
+  setTimeout(function() {
+      var markers = document.querySelectorAll('h1.marker');
+      markers.forEach(function(marker) {
+          marker.classList.add('active-divider');
+      });
+  }, 1000); // Thời gian chờ là 1000ms (1 giây)
+});
 
 
 })();
